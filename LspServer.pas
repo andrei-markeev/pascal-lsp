54716d4ab0dc25f79e7927563658ba70e7fa60b8
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2026-07-17 20:19:19 +0300

    Go to definition

--- LspServer.pas
+++ LspServer.pas
@@ -109,6 +109,18 @@ begin
   Result := StringReplace(Result, '%3a', ':', [rfReplaceAll]);
 end;
 
+function FilenameToUri(const Filename: string): string;
+var
+  UnixPath: string;
+begin
+  UnixPath := StringReplace(Filename, '\', '/', [rfReplaceAll]);
+  UnixPath := StringReplace(UnixPath, ' ', '%20', [rfReplaceAll]);
+  if (Length(UnixPath) > 1) and (UnixPath[2] = ':') then
+    Result := 'file:///' + UnixPath
+  else
+    Result := 'file://' + UnixPath;
+end;
+
 procedure HandleFileChange(WriteStream: TStream; const Uri: string; const Content: string);
 var
   ctx: TParserContext;
@@ -662,6 +674,95 @@ begin
   SendResponse(WriteStream, Response);
 end;
 
+procedure HandleDefinition(WriteStream: TStream; Id: TJSONData; Params: TJSONData);
+var
+  Uri, DeclUri, ResultJson, Response: string;
+  TargetLine, TargetCharacter: integer;
+  TargetIdent, DeclIdent: TIdentifier;
+  i: integer;
+  curToken: TToken;
+  Sym: TSymbol;
+  DeclCtx: TParserContext;
+  UnitName: string;
+begin
+  Response := '{"jsonrpc":"2.0",';
+  if Id <> nil then
+    Response := Response + '"id":' + Id.AsJSON + ','
+  else
+    Response := Response + '"id":null,';
+
+  ResultJson := 'null';
+
+  if (Params <> nil) and (LastParserContext <> nil) then
+  begin
+    Uri := Params.FindPath('textDocument.uri').AsString;
+    TargetLine := Params.FindPath('position.line').AsInteger;
+    TargetCharacter := Params.FindPath('position.character').AsInteger;
+
+    if LastParsedUri = Uri then
+    begin
+      TargetIdent := nil;
+      for i := 0 to LastParserContext.tokensLen - 1 do
+      begin
+        curToken := LastParserContext.Tokens[i];
+        if (curToken <> nil) and (curToken is TIdentifier) then
+        begin
+          if (curToken.line = TargetLine) and
+             (TargetCharacter >= curToken.position) and
+             (TargetCharacter <= curToken.position + curToken.len) then
+          begin
+            TargetIdent := TIdentifier(curToken);
+            Break;
+          end;
+        end;
+      end;
+
+      if TargetIdent <> nil then
+      begin
+        if TargetIdent.symbol <> nil then
+        begin
+          Sym := TSymbol(TargetIdent.symbol);
+          if Sym.declaration <> nil then
+          begin
+            DeclIdent := Sym.declaration;
+            DeclCtx := FindContextForCursor(DeclIdent.start);
+            if DeclCtx <> nil then
+            begin
+              DeclUri := FilenameToUri(DeclCtx.filePath);
+              ResultJson := '{' +
+                '"uri":"' + string(StringToJSONString(DeclUri)) + '",' +
+                '"range":{' +
+                  '"start":{"line":' + IntToStr(DeclIdent.line) + ',"character":' + IntToStr(DeclIdent.position) + '},' +
+                  '"end":{"line":' + IntToStr(DeclIdent.line) + ',"character":' + IntToStr(DeclIdent.position + DeclIdent.len) + '}' +
+                '}' +
+              '}';
+            end;
+          end;
+        end
+        else
+        begin
+          UnitName := LowerCase(TargetIdent.GetStr());
+          DeclCtx := TParserContext(LoadedUnits.Find(UnitName));
+          if DeclCtx <> nil then
+          begin
+            DeclUri := FilenameToUri(DeclCtx.filePath);
+            ResultJson := '{' +
+              '"uri":"' + string(StringToJSONString(DeclUri)) + '",' +
+              '"range":{' +
+                '"start":{"line":0,"character":0},' +
+                '"end":{"line":0,"character":0}' +
+              '}' +
+            '}';
+          end;
+        end;
+      end;
+    end;
+  end;
+
+  Response := Response + '"result":' + ResultJson + '}';
+  SendResponse(WriteStream, Response);
+end;
+
 procedure ProcessRequest(WriteStream: TStream; const JsonStr: string);
 var
   Json: TJSONData;
@@ -744,6 +845,7 @@ begin
         '"capabilities":{' +
           '"textDocumentSync":1,' +
           '"documentSymbolProvider":true,' +
+          '"definitionProvider":true,' +
           '"semanticTokensProvider":{' +
             '"legend":{' +
               '"tokenTypes":["namespace","class","interface","struct","type","parameter","variable","property","function","method","keyword","number","string","comment","operator"],' +
@@ -771,6 +873,10 @@ begin
     begin
       Uri := Json.FindPath('params.textDocument.uri').AsString;
       HandleDocumentSymbol(WriteStream, Uri, Id);
+    end
+    else if Method = 'textDocument/definition' then
+    begin
+      HandleDefinition(WriteStream, Id, Json.FindPath('params'));
     end
     else if Method = 'textDocument/semanticTokens/full' then
     begin
